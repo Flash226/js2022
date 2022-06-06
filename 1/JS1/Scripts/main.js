dbfba8b0f50d48f3e34a8bcbc79e3937b464ad84
Flash226 <slavanepuk@gmail.com>
2022-06-06 22:54:49 +0200

    commit on main

--- 1/JS1/Scripts/main.js
+++ 1/JS1/Scripts/main.js
@@ -2,16 +2,14 @@ const elOne = document.querySelector(".one");
 const elTwo = document.querySelector(".two");
 const elThree = document.querySelector(".three");
 
-
-
-document.querySelector(".one").addEventListener("click", function(){
+document.querySelector(".one").addEventListener("click", function () {
     document.getElementById("one").style.backgroundColor = "yellow";
-    });
+});
 
-document.querySelector(".two").addEventListener("click", function(){
+document.querySelector(".two").addEventListener("click", function () {
     document.getElementById("two").style.backgroundColor = "yellow";
-    });
+});
 
-document.querySelector(".three").addEventListener("click", function(){
+document.querySelector(".three").addEventListener("click", function () {
     document.getElementById("three").style.backgroundColor = "yellow";
-    });
+});
