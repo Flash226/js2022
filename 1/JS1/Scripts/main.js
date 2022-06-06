d87920a8c58cb967c18b85736faca09ac443ff04
Flash226 <slavanepuk@gmail.com>
2022-06-06 22:43:02 +0200

    commit on main

--- 1/JS1/Scripts/main.js
+++ 1/JS1/Scripts/main.js
@@ -6,12 +6,12 @@ const elThree = document.querySelector(".three");
 
 document.querySelector(".one").addEventListener("click", function(){
     document.getElementById("one").style.backgroundColor = "yellow";
-});
+    });
 
 document.querySelector(".two").addEventListener("click", function(){
     document.getElementById("two").style.backgroundColor = "yellow";
-});
+    });
 
 document.querySelector(".three").addEventListener("click", function(){
     document.getElementById("three").style.backgroundColor = "yellow";
-});
+    });
