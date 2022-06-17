65e6583bfa2e1e80c1e9290ace9e1dc5f041d5d8
Flash226 <slavanepuk@gmail.com>
2022-06-17 15:48:07 +0200

    commit on main

--- 1/JS5/16.06/1/scripts/main.js
+++ 1/JS5/16.06/1/scripts/main.js
@@ -9,7 +9,6 @@ const fio = () => {
   let val2 = input2.value;
   let val3 = input3.value;
   if ((/\W/.test(val1) === true && (/\'/.test(val1) != true)) || (/\d/.test(val1) === true) || (/\W/.test(val2) === true && (/\'/.test(val2) != true)) || (/\d/.test(val2) === true) || (/\W/.test(val3) === true && (/\'/.test(val3) != true)) || (/\d/.test(val3) === true)) {
-    console.log(val1.search);
     result2.innerHTML = 'Check the correctness';
     result2.style.backgroundColor = 'red';
     return;
