3abf08e966522065b74d5b56d3bdfec74661c362
Flash226 <slavanepuk@gmail.com>
2022-08-15 10:22:34 +0200

    commit on main

--- 1/JS15/2/public/script/script.js
+++ 1/JS15/2/public/script/script.js
@@ -5,7 +5,7 @@ const sendGetRequest = async () => {
     for (i = 0; i < res.data.length; i++) {
         let card = document.createElement('div');
         card.classList.add('card');
-        card.innerHTML = `<div class="name">${res.data[i].brand} ${res.data[i].model}</div>
+        card.innerHTML = `<div class="name"><h3>${res.data[i].brand} ${res.data[i].model}</h3></div>
      <div class="pic"><img class "imgCard" src="${res.data[i].picture}" alt="${res.data[i].brand} ${res.data[i].model}" height="180" ></div>
      <div class="price">Price: ${res.data[i].price}</div>
      <div class="link"><a href="./product/${res.data[i].id}">Details...</a></div>`
