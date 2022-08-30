6c7f4db643a481e2378767d0058d4af664ccc4cc
Flash226 <slavanepuk@gmail.com>
2022-08-30 06:52:58 +0200

    commit on main

--- 1/JS15/3/public/js/main.js
+++
@@ -1,5 +0,0 @@
-const initProduct = async () => {
-    const result = await axios.get('./product.json')
-    console.log(result.data);
-}
-initProduct();
---
+++ 1/JS16/2/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const multer = require('multer');
+
+const serv = express();
+const PORT = process.env.PORT ?? 3000;
+
+serv.set('view engine', 'ejs');
+serv.set('views', './views/');
+
+serv.use(express.static(__dirname + './public'));
+
+const upload = multer({ dest: "public/uploads/" });
+
+serv.use(express.json());
+serv.use(express.urlencoded({ extended: true }));
+
+
+serv.post("/test", upload.single("file"), uploadFile);
+
+function uploadFile(req, res) {
+    console.log(req.body);
+    console.log(req.files);
+    res.json({ message: "Successfully uploaded files" });
+}
+
+
+
+
+
+serv.get('/', (req, res) => {
+    res.render('main');
+});
+
+// serv.post('/test', (req, res) => {
+//     console.log('work')
+//     res.json({status: 'ok'});
+// });
+
+
+serv.listen(PORT, () => 
+console.log(`Server has been started on port ${PORT}...`));
